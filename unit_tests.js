ad2238b4b45750a7e39f31433f986eba6d94d047
ghirschhorn987 <ghirschhorn987@users.noreply.github.com>
2025-09-05 13:31:56 +0000

    More unit tests

--- unit_tests.js
+++ unit_tests.js
@@ -4,7 +4,7 @@ function runAllTests() {
   Logger.log("Starting test suite...");
   
   testGetRosterMap();
-  testAnotherFeature();
+  testGetMainRosterPlayers();
 
   Logger.log("Test suite finished.");
 }
@@ -25,7 +25,8 @@ function testGetRosterMap() {
                 ["", ""],
                 ["player4 <email4@d.com>", ""],
                 ["player5 <email5@e.com>", "SecondaryReserve"],
-                ["player6 <email6@f.com>", "Main"]
+                ["player6 <email6@f.com>", "Main"],
+                ["emailonly@g.com", "Main"]
               ];
             }
           };
@@ -40,8 +41,8 @@ function testGetRosterMap() {
   const mainPlayers = rosterMap[PLAYER_TYPE_MAIN];
   const secondaryPlayers = rosterMap[PLAYER_TYPE_SECONDARY_RESERVE];
 
-  if (!mainPlayers || mainPlayers.length !== 3) {
-    Logger.log("Test Failed: Expected 3 main players, but found " + (mainPlayers ? mainPlayers.length : 0));
+  if (!mainPlayers || mainPlayers.length !== 4) {
+    Logger.log("Test Failed: Expected 4 main players, but found " + (mainPlayers ? mainPlayers.length : 0));
   } else {
     Logger.log("Test Passed: Correct number of main players.");
   }
@@ -52,7 +53,7 @@ function testGetRosterMap() {
     Logger.log("Test Passed: Correct number of secondary players.");
   }
 
-  const expectedMain = ["player1 <email1@a.com>", "player3 <email3@c.com>", "player6 <email6@f.com>"];
+  const expectedMain = ["player1 <email1@a.com>", "player3 <email3@c.com>", "player6 <email6@f.com>", "emailonly@g.com"];
   if (JSON.stringify(mainPlayers) !== JSON.stringify(expectedMain)) {
       Logger.log("Test Failed: Main player list does not match expected.");
       Logger.log("Expected: " + JSON.stringify(expectedMain));
@@ -65,8 +66,18 @@ function testGetRosterMap() {
   SpreadsheetApp = originalSpreadsheetApp;
 }
 
-function testAnotherFeature() {
-    Logger.log("Running test: testAnotherFeature...");
-    // Your test logic here
-    Logger.log("Test Passed: Another feature works as expected.");
+function testGetMainRosterPlayers() {
+    Logger.log("Running test: testGetMainRosterPlayers...");
+    const rosterMap = {
+        "Main": ["player1", "player2"],
+        "SecondaryReserve": ["player3"]
+    };
+
+    const mainPlayers = getMainRosterPlayers(rosterMap);
+
+    if (mainPlayers.length !== 2 || mainPlayers[0] !== "player1" || mainPlayers[1] !== "player2") {
+        Logger.log("Test Failed: getMainRosterPlayers did not return the correct players.");
+    } else {
+        Logger.log("Test Passed: getMainRosterPlayers works as expected.");
+    }
 }
